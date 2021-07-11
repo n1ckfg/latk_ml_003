a39ad9637decd8baa856e96f23eb488d38f4bd80
Nick Fox-Gieg <nick@fox-gieg.com>
2021-07-11 16:08:18 -0400

    converter shell scripts

--- binvox_to_h5.py
+++ binvox_to_h5.py
@@ -2,23 +2,17 @@
 # https://github.com/raahii/3dgan-chainer/blob/master/LICENSE
 # Convert dataset(.binvox) to numpy array in advance
 # to load dataset more efficient.
-#
+
 import sys, os, glob
-#from common.data_io import read_binvox, read_h5
 import numpy as np
 import scipy.ndimage as nd
 import h5py
 import binvox_rw
 
-def read_h5(path):
-    """
-    read .h5 file
-    """
-    f = h5py.File(path, 'r')
-    voxel = f['data'][:]
-    f.close()
+argv = sys.argv
+argv = argv[argv.index("--") + 1:] # get all args after "--"
 
-    return voxel
+inputPath = argv[0]
 
 def resize(voxel, shape):
     """
@@ -32,7 +26,7 @@ def resize(voxel, shape):
     voxel[np.nonzero(voxel)] = 1.0
     return voxel
 
-def read_binvox(path, shape=(64,64,64), fix_coords=True):
+def read_binvox(path, shape=(128,128,128), fix_coords=True):
     """
     read voxel data from .binvox file
     """
@@ -45,31 +39,6 @@ def read_binvox(path, shape=(64,64,64), fix_coords=True):
 
     return voxel_data
 
-def write_binvox(data, path):
-    """
-    write out voxel data
-    """
-    data = np.rint(data).astype(np.uint8)
-    dims = data.shape
-    translate = [0., 0., 0.]
-    scale = 1.0
-    axis_order = 'xyz'
-    v = binvox_rw.Voxels( data, dims, translate, scale, axis_order)
-
-    with open(path, 'bw') as f:
-        v.write(f)
-
-def read_all_binvox(directory):
-    """
-    read all .binvox files in the direcotry
-    """
-    input_files = [f for f in glob.glob(directory + "/**/*.binvox", recursive=True)]
-
-    data = np.array([read_binvox(path) for path in input_files])
-    n, w, h, d = data.shape
-
-    return data.reshape(n, w, h, d, 1)
-
 # ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 def convert_h5(in_path, out_path):
     data = read_binvox(in_path)
@@ -81,42 +50,16 @@ def convert_h5(in_path, out_path):
 
     return out_path
 
-def check_open_h5(save_dir):
-    if save_dir[-1] != '/':
-        save_dir += '/'
-
-    files = [f for f in glob.glob(save_dir + "**/*.h5", recursive=True)]
-    for path in files:
-        try:
-            read_h5(path)
-        except KeyError:
-            print(path)
-
-def extract_name(path):
-    return path.split('/')[-1].split('.')[0]
-
 def main():
-    if len(sys.argv) < 3:
-        print("usage: python dump.py dataset_path(.binvox) save_dir")
-        sys.exit()
-    
-    dataset_path = sys.argv[1]
-    save_dir = sys.argv[2]
-    
-    if dataset_path[-1] != '/':
-        dataset_path += '/'
-    if save_dir[-1] != '/':
-        save_dir += '/'
+    print("Reading from : " + inputPath)
 
-    os.makedirs(save_dir, exist_ok=True)
-    files = [f for f in glob.glob(dataset_path + "**/*.binvox", recursive=True)]
-    print("{} files found.".format(len(files)))
-    
-    ext = sys.argv[1]
-    for in_path in files:
-        out_path = save_dir + extract_name(in_path) + '.h5'
-        result = convert_h5(in_path, out_path)
+    url = ""
+    outputPathArray = inputPath.split(".")
+    for i in range(0, len(outputPathArray)-1):
+        url += outputPathArray[i]
+    url += ".im"
 
-if __name__=="__main__":
-    # main()
-    check_open_h5(sys.argv[1])
+    print("Writing to: " + url)
+    convert_h5(inputPath, url)
+
+main()
