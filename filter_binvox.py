2d2485a0e185a8bdf6142de1a39fbc4e38a72320
Nick Fox-Gieg <nick@fox-gieg.com>
2021-07-16 00:30:52 -0400

    Update filter_binvox.py

--- filter_binvox.py
+++ filter_binvox.py
@@ -16,10 +16,12 @@ inputPath = argv[0]
 dims = 128
 
 # filters
-dilateReps = 3
-erodeReps = 2
-gaussianSigma = 0
-medianSize = 3
+dilateReps = 0 #3
+erodeReps = 2 #2
+gaussianSigma = 0 #0
+medianSize = 4 #3
+sobelReps = 2 #0
+laplaceReps = 1 #0
 
 def read_binvox(path, shape=(dims, dims, dims), fix_coords=True):
     voxel = None
@@ -46,14 +48,24 @@ def main():
         for i in range(0, dilateReps):
             nd.binary_dilation(bv.data.copy(), output=bv.data)
 
-	if (gaussianSigma > 0):
-	    print("Gaussian filter")
-	    nd.gaussian_filter(bv.data.copy(), sigma=gaussianSigma, output=bv.data)
-	
-	if (medianSize > 0):
-	    print("Median filter")
-	    nd.median_filter(bv.data.copy(), size=medianSize, output=bv.data)
+    if (sobelReps > 0):
+    	print ("Sobel filter...")
+    	for i in range(0, sobelReps):
+    		nd.sobel(bv.data.copy(), output=bv.data)
+
+    if (gaussianSigma > 0):
+        print("Gaussian filter")
+        nd.gaussian_filter(bv.data.copy(), sigma=gaussianSigma, output=bv.data)
     
+    if (medianSize > 0):
+        print("Median filter")
+        nd.median_filter(bv.data.copy(), size=medianSize, output=bv.data)
+    
+    if (laplaceReps > 0):
+    	print("Laplace filter...")
+    	for i in range(0, laplaceReps):
+    		nd.laplace(bv.data.copy(), output=bv.data)
+
     if (erodeReps > 0):
         print("Eroding...")
         for i in range(0, erodeReps):
