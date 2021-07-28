c776fc6f0cb6844005af2299782e6a000f8c5114
Nick Fox-Gieg <nick@fox-gieg.com>
2021-07-28 16:41:02 -0400

    color transfer script

---
+++ color_transfer.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import pymeshlab as ml
+
+def main():
+    argv = sys.argv
+    argv = argv[argv.index("--") + 1:] # get all args after "--"
+
+    inputPath1 = argv[0]
+    newPathBase = os.path.basename(inputPath1)[:-4]
+    inputPath2 = os.path.join(argv[1], newPathBase + "_fake_filter_post_edges.ply")
+    outputPath = os.path.join(argv[1], newPathBase + "_final.ply")
+
+    ms = ml.MeshSet()
+    ms.load_new_mesh(inputPath1)
+    ms.load_new_mesh(inputPath2)
+    
+    # https://pymeshlab.readthedocs.io/en/latest/filter_list.html    
+    ms.apply_filter("vertex_attribute_transfer", sourcemesh=0, targetmesh=1)
+    ms.save_current_mesh(outputPath)
+
+main()
