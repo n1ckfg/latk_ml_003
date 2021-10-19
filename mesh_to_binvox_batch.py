cf465d96b7243d74991da2958d63bad350a8f3ca
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-19 15:45:36 -0400

    Update mesh_to_binvox_batch.py

--- mesh_to_binvox_batch.py
+++ mesh_to_binvox_batch.py
@@ -88,7 +88,7 @@ def main():
         localNorms.append(normVals)
 
     for i, url in enumerate(urls):
-        #mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter, normVals=localNorms[i], dimVals=localDims[i])
-        mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter)
+        #mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter)
+        mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter, normVals=localNorms[i], dimVals=localDims[i])
 
 main()
