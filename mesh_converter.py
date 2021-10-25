00e6d0f375890ba8354b0eadd4a63ba3c7b301b1
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-25 11:06:07 -0400

    Update mesh_converter.py

--- mesh_converter.py
+++ mesh_converter.py
@@ -12,38 +12,14 @@ def scale_numpy_array(arr, min_v, max_v):
     scaled_unit = (max_range - min_range) / (np.max(arr) - np.min(arr))
     return arr * scaled_unit - np.min(arr) * scaled_unit + min_range
 
-# https://codereview.stackexchange.com/questions/185785/scale-numpy-array-to-certain-range
-'''
-def interp(val, out_range):
-    return out_range[0] * (1.0 - val) + out_range[1] * val
-
-def uninterp(val, domain):
-    b = 0
-    if (domain[1] - domain[0]) != 0:
-        b = domain[1] - domain[0]
-    else:
-        b =  1.0 / domain[1]
-    return (val - domain[0]) / b
-
-def scale_numpy_array_axes(arr, rangeX=(0, 1), rangeY=(0, 1), rangeZ=(0, 1)):
-    returns = arr
-
-    domainX = [np.min(returns, axis=0), np.max(returns, axis=0)]
-    domainY = [np.min(returns, axis=1), np.max(returns, axis=1)]
-    domainZ = [np.min(returns, axis=2), np.max(returns, axis=2)]
-
-    returns = interp(uninterp(returns, domainX), rangeX)
-    returns = interp(uninterp(returns, domainY), rangeY)
-    returns = interp(uninterp(returns, domainZ), rangeZ)
-
-    return returns
-'''
-
 def remap(value, min1, max1, min2, max2):
+    '''
     range1 = max1 - min1
     range2 = max2 - min2
     valueScaled = float(value - min1) / float(range1)
     return min2 + (valueScaled * range2)
+    '''
+    return np.interp(value,[min1, max1],[min2, max2])
 
 def binvoxToMesh(url, ext="_post.ply", dims=128, axis='xyz'):
     voxel = None
@@ -74,25 +50,6 @@ def meshToBinvox(url, ext="_pre.ply", dims=128, doFilter=False, normVals=None, d
             vert[0] = remap(vert[0], dimVals[0], dimVals[1], dims * normVals[0], (dims * normVals[1]) - 1)
             vert[1] = remap(vert[1], dimVals[2], dimVals[3], dims * normVals[2], (dims * normVals[3]) - 1)
             vert[2] = remap(vert[2], dimVals[4], dimVals[5], dims * normVals[4], (dims * normVals[5]) - 1)
-        '''
-        vertsX = []
-        vertsY = []
-        vertsZ = []
-
-        for vert in mesh.vertices:
-            vertsX.append(vert[0])
-            vertsY.append(vert[1])
-            vertsZ.append(vert[2])
-
-        vertsX = scale_numpy_array(np.array(vertsX), dims * normVals[0], dims * normVals[1] - 1)
-        vertsY = scale_numpy_array(np.array(vertsY), dims * normVals[2], dims * normVals[3] - 1)
-        vertsZ = scale_numpy_array(np.array(vertsZ), dims * normVals[4], dims * normVals[5] - 1)
-
-        for i, vert in enumerate(mesh.vertices):
-            vert[0] = vertsX[i]
-            vert[1] = vertsY[i]
-            vert[2] = vertsZ[i]
-        '''
     else:
         mesh.vertices = scale_numpy_array(mesh.vertices, 0, dims - 1)
 
