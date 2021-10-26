473a693225100866d9cc5b9a168465d9a67b22d6
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-26 18:21:12 -0400

    begin refactor

--- mesh_to_binvox.py
+++ mesh_to_binvox.py
@@ -1,6 +1,8 @@
 import sys
+import os
 import distutils.util
 import mesh_converter as mc
+import trimesh
 
 def main():
     argv = sys.argv
@@ -8,9 +10,86 @@ def main():
 
     inputPath = argv[0]
     inputExt = argv[1]
-    dims = int(argv[2])
-    doFilter = bool(distutils.util.strtobool(argv[3]))
+    outputExt = argv[2]
+    dims = int(argv[3])
+    doFilter = bool(distutils.util.strtobool(argv[4]))
 
-    mc.meshToBinvox(url=inputPath, ext=inputExt, dims=dims, doFilter=doFilter)
+    seqMinX = 0
+    seqMaxX = 0
+    seqMinY = 0
+    seqMaxY = 0
+    seqMinZ = 0
+    seqMaxZ = 0
+
+    localDims = []
+    localNorms = []
+    urls = []
+
+    for fileName in os.listdir(inputPath):
+        if fileName.endswith(inputExt): 
+            url = os.path.join(inputPath, fileName)
+            print("Finding bounds for " + url)
+
+            urls.append(url)
+            mesh = trimesh.load(url)
+            
+            minX = 0
+            maxX = 0
+            minY = 0
+            maxY = 0
+            minZ = 0
+            maxZ = 0
+            
+            for vert in mesh.vertices:
+                x = vert[0]
+                y = vert[1]
+                z = vert[2]
+                if (x < minX):
+                    minX = x
+                if (x > maxX):
+                    maxX = x
+                if (y < minY):
+                    minY = y
+                if (y > maxY):
+                    maxY = y
+                if (z < minZ):
+                    minZ = z
+                if (z > maxZ):
+                    maxZ = z
+
+            localDim = (minX, maxX, minY, maxY, minZ, maxZ)
+            #print(localDim)
+            localDims.append(localDim)
+
+            if (minX < seqMinX):
+                seqMinX = minX
+            if (maxX > seqMaxX):
+                seqMaxX = maxX
+            if (minY < seqMinY):
+                seqMinY = minY
+            if (maxY > seqMaxY):
+                seqMaxY = maxY
+            if (minZ < seqMinZ):
+                seqMinZ = minZ
+            if (maxZ > seqMaxZ):
+                seqMaxZ = maxZ
+
+    for localDim in localDims:
+        normMinX = mc.remap(localDim[0], seqMinX, seqMaxX, 0, 1)
+        normMaxX = mc.remap(localDim[1], seqMinX, seqMaxX, 0, 1)
+        normMinY = mc.remap(localDim[2], seqMinY, seqMaxY, 0, 1)
+        normMaxY = mc.remap(localDim[3], seqMinY, seqMaxY, 0, 1)
+        normMinZ = mc.remap(localDim[4], seqMinZ, seqMaxZ, 0, 1)
+        normMaxZ = mc.remap(localDim[5], seqMinZ, seqMaxZ, 0, 1)
+
+        normVals = (normMinX, normMaxX, normMinY, normMaxY, normMinZ, normMaxZ)
+        #print(normVals)
+
+        localNorms.append(normVals)
+
+    for i, url in enumerate(urls):
+        print("Converting to binvox: " + url)
+        #mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter)
+        mc.meshToBinvox(url=url, ext=outputExt, dims=dims, doFilter=doFilter, normVals=localNorms[i], dimVals=localDims[i])
 
 main()
--- pipeline001.sh
+++ pipeline001.sh
@@ -10,34 +10,36 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 cd $DIR
 
-DIMS=$1
-EPOCH=$2
-RESAMPLE=$3
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+DIMS=$3
+EPOCH=$4
+RESAMPLE=$5
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~
 echo "1. Preprocessing..."
 echo "1.1. Resample point clouds."
-./mesh_resample.sh "input" "$RESAMPLE" ".obj" "_resample.ply"
+./mesh_resample.sh "$INPUT_DIR" "$RESAMPLE" ".obj" "_resample.ply"
 
 echo "1.2. Convert point clouds to voxel grids."
-./mesh_to_binvox_batch.sh "input" "_resample.ply" "_pre.ply" "$DIMS" "True" # *_pre.ply -> *.binvox
-./binvox_to_h5.sh "input" "$DIMS" # *.binvox -> *.im
+python mesh_to_binvox.py -- "$INPUT_DIR" "_resample.ply" "_pre.ply" "$DIMS" "True"  # *_pre.ply -> *.binvox
+./binvox_to_h5.sh "$INPUT_DIR" "$DIMS" # *.binvox -> *.im
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~
 echo "2. Inference..."
-python test.py --epoch "$EPOCH" --dataset "input" --img_width "$DIMS" --img_height "$DIMS" --img_depth "$DIMS"
+python test.py --epoch "$EPOCH" --dataset "$INPUT_DIR" --img_width "$DIMS" --img_height "$DIMS" --img_depth "$DIMS"
 
 # ~ ~ ~ ~ ~ ~ ~ ~ ~
 echo "3. Postprocessing..."
 echo "3.1. Filter output."
-./filter_binvox.sh "output" "$DIMS" # *_fake.binvox -> *_fake_filter.binvox
+./filter_binvox.sh "$OUTPUT_DIR" "$DIMS" # *_fake.binvox -> *_fake_filter.binvox
 rm output/*fake.binvox
 
 echo "3.2. Convert voxel grids to point clouds."
-./binvox_to_mesh.sh "output" "$DIMS" # *_fake_filter.binvox -> _post.ply
+./binvox_to_mesh.sh "$OUTPUT_DIR" "$DIMS" # *_fake_filter.binvox -> _post.ply
 
 echo "3.3. Find edges in point clouds."
-./Difference_Eigenvalues.sh "output" # *_post.ply -> *_post_edges.ply
+./Difference_Eigenvalues.sh "$OUTPUT_DIR" # *_post.ply -> *_post_edges.ply
 
 echo "3.4. Transfer vertex color."
-./color_transfer.sh "input" "output" "_resample_fake_filter_post_edges.ply" # -> *final.obj
+./color_transfer.sh "$INPUT_DIR" "$OUTPUT_DIR" "_resample_fake_filter_post_edges.ply" # -> *final.obj
