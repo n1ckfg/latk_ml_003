48c9cb3725968b3d93796a54dcd0de9b4afc42c2
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-27 01:29:18 -0400

    Update filter_binvox.py

--- filter_binvox.py
+++ filter_binvox.py
@@ -33,49 +33,49 @@ def main():
     inputPath = argv[0]
     dims = int(argv[1])
 
-for fileName in os.listdir(inputPath):
-    if fileName.endswith(".binvox"): 
-        inputUrl = os.path.join(inputPath, fileName)
+    for fileName in os.listdir(inputPath):
+        if fileName.endswith(".binvox"): 
+            inputUrl = os.path.join(inputPath, fileName)
 
-        print("Reading from : " + inputUrl)
-        bv = read_binvox(inputUrl)
+            print("Reading from : " + inputUrl)
+            bv = read_binvox(inputUrl)
 
-        outputUrl = ""
-        outputPathArray = inputUrl.split(".")
-        for i in range(0, len(outputPathArray)-1):
-            outputUrl += outputPathArray[i]
-        outputUrl += "_filter.binvox"
-       
-        # filters
-        if (dilateReps > 0):
-            print("Dilating...")
-            for i in range(0, dilateReps):
-                nd.binary_dilation(bv.data.copy(), output=bv.data)
+            outputUrl = ""
+            outputPathArray = inputUrl.split(".")
+            for i in range(0, len(outputPathArray)-1):
+                outputUrl += outputPathArray[i]
+            outputUrl += "_filter.binvox"
+           
+            # filters
+            if (dilateReps > 0):
+                print("Dilating...")
+                for i in range(0, dilateReps):
+                    nd.binary_dilation(bv.data.copy(), output=bv.data)
 
-        if (sobelReps > 0):
-            print ("Sobel filter...")
-            for i in range(0, sobelReps):
-                nd.sobel(bv.data.copy(), output=bv.data)
+            if (sobelReps > 0):
+                print ("Sobel filter...")
+                for i in range(0, sobelReps):
+                    nd.sobel(bv.data.copy(), output=bv.data)
 
-        if (gaussianSigma > 0):
-            print("Gaussian filter")
-            nd.gaussian_filter(bv.data.copy(), sigma=gaussianSigma, output=bv.data)
-        
-        if (medianSize > 0):
-            print("Median filter")
-            nd.median_filter(bv.data.copy(), size=medianSize, output=bv.data)
-        
-        if (laplaceReps > 0):
-            print("Laplace filter...")
-            for i in range(0, laplaceReps):
-                nd.laplace(bv.data.copy(), output=bv.data)
+            if (gaussianSigma > 0):
+                print("Gaussian filter")
+                nd.gaussian_filter(bv.data.copy(), sigma=gaussianSigma, output=bv.data)
+            
+            if (medianSize > 0):
+                print("Median filter")
+                nd.median_filter(bv.data.copy(), size=medianSize, output=bv.data)
+            
+            if (laplaceReps > 0):
+                print("Laplace filter...")
+                for i in range(0, laplaceReps):
+                    nd.laplace(bv.data.copy(), output=bv.data)
 
-        if (erodeReps > 0):
-            print("Eroding...")
-            for i in range(0, erodeReps):
-                nd.binary_erosion(bv.data.copy(), output=bv.data)
+            if (erodeReps > 0):
+                print("Eroding...")
+                for i in range(0, erodeReps):
+                    nd.binary_erosion(bv.data.copy(), output=bv.data)
 
-        print("Writing to: " + outputUrl)
-        write_binvox(bv, outputUrl)
+            print("Writing to: " + outputUrl)
+            write_binvox(bv, outputUrl)
     
 main()
