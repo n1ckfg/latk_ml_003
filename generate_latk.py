8e1aabe0f8b91673816a788968643925cf6cd042
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-09 00:32:43 -0500

    test

---
+++ generate_latk.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import latk
+import trimesh
+
+def main():
+    argv = sys.argv
+    argv = argv[argv.index("--") + 1:] # get all args after "--"
+
+    inputPath1 = argv[0]
+
+    for fileName in os.listdir(inputPath1):
+        if fileName.endswith(argv[1]): 
+            url = os.path.join(inputPath1, fileName)
+
+            mesh = trimesh.load(url)
+
+            points = []
+
+            for 
+
+main()
