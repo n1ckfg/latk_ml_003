3f7403b91ca8c973ec0fbd675bb0d9ee0a6f89d6
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-10 00:32:26 -0500

    Update generate_latk.py

--- generate_latk.py
+++ generate_latk.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import latk
-import trimesh
+import pymeshlab as ml
 
 def main():
     argv = sys.argv
@@ -9,12 +9,30 @@ def main():
 
     inputPath1 = argv[0]
 
+	la = latk.Latk()
+	layer = latk.LatkLayer()
+	la.layers.append(layer)
+
+	frameCounter = 0
+
     for fileName in os.listdir(inputPath1):
         if fileName.endswith(argv[1]): 
             url = os.path.join(inputPath1, fileName)
 
-            mesh = trimesh.load(url)
+			ms = ml.MeshSet()
+			ms.load_new_mesh(url)
+			mesh = ms.current_mesh()
+			vertices = mesh.vertex_matrix()
 
-            points = []
+			points = []
+			for vert in vertices:
+				point = latk.LatkPoint((vert[0], vert[1], vert[2]))
+				points.append(point)
+			stroke = latk.LatkStroke(points)
+			frame = latk.LatkFrame(stroke)
+
+			la.layers[0].frames.append(frame)
+
+	la.write("output.latk")
 
 main()
