af9002f5f756434717bd34a30ee0cffffb1b4167
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-10 01:28:41 -0500

    Update generate_latk.py

--- generate_latk.py
+++ generate_latk.py
@@ -9,30 +9,30 @@ def main():
 
     inputPath1 = argv[0]
 
-	la = latk.Latk()
-	layer = latk.LatkLayer()
-	la.layers.append(layer)
+    la = latk.Latk()
+    layer = latk.LatkLayer()
+    la.layers.append(layer)
 
-	frameCounter = 0
+    frameCounter = 0
 
     for fileName in os.listdir(inputPath1):
         if fileName.endswith(argv[1]): 
             url = os.path.join(inputPath1, fileName)
 
-			ms = ml.MeshSet()
-			ms.load_new_mesh(url)
-			mesh = ms.current_mesh()
-			vertices = mesh.vertex_matrix()
+            ms = ml.MeshSet()
+            ms.load_new_mesh(url)
+            mesh = ms.current_mesh()
+            vertices = mesh.vertex_matrix()
 
-			points = []
-			for vert in vertices:
-				point = latk.LatkPoint((vert[0], vert[1], vert[2]))
-				points.append(point)
-			stroke = latk.LatkStroke(points)
-			frame = latk.LatkFrame(stroke)
+            points = []
+            for vert in vertices:
+                point = latk.LatkPoint((vert[0], vert[1], vert[2]))
+                points.append(point)
+            stroke = latk.LatkStroke(points)
+            frame = latk.LatkFrame(stroke)
 
-			la.layers[0].frames.append(frame)
+            la.layers[0].frames.append(frame)
 
-	la.write("output.latk")
+    la.write("output.latk")
 
 main()
