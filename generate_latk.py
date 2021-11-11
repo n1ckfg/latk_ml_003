17ce295e9109dcf3b6696c1d732a40dd2b263f01
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-10 19:51:10 -0500

    Update generate_latk.py

--- generate_latk.py
+++ generate_latk.py
@@ -17,60 +17,65 @@ def main():
     frameCounter = 0
     maxStrokePoints = 50
 
+    urls = []
     for fileName in os.listdir(inputPath1):
         if fileName.endswith(argv[1]): 
             url = os.path.join(inputPath1, fileName)
-            print("Loading from " + url)
+            urls.append(url)
+            urls.sort()
 
-            ms = ml.MeshSet()
-            ms.load_new_mesh(url)
-            mesh = ms.current_mesh()
-            vertices = mesh.vertex_matrix()
-            print("Found " + str(len(vertices)) + " vertices")
+    for url in urls:
+        print("Loading from " + url)
 
-            points = []
-            for vert in vertices:
-                point = latk.LatkPoint((vert[0], vert[2], vert[1]))
-                points.append(point)
+        ms = ml.MeshSet()
+        ms.load_new_mesh(url)
+        mesh = ms.current_mesh()
+        vertices = mesh.vertex_matrix()
+        print("Found " + str(len(vertices)) + " vertices")
 
-            numStrokes = int(len(points) / maxStrokePoints) * 2
-            strokes = []
+        points = []
+        for vert in vertices:
+            point = latk.LatkPoint((vert[0], vert[2], vert[1]))
+            points.append(point)
+
+        numStrokes = int(len(points) / maxStrokePoints) * 2
+        strokes = []
+        
+        # MEDIAN
+        for i in range(0, numStrokes):
+            startIndex = int(rnd(0, len(points)))
+            startPoint = points[startIndex]
+
+            medianDistance = 0
+
+            for point in points:
+                point.distance = la.getDistance(point.co, startPoint.co)
+                if point.distance > medianDistance:
+                    medianDistance = point.distance
+
+            medianDistance /= 2
+
+            points = sorted(points, key=lambda x: getattr(x, 'distance'))
+
+            newPoints = []
+            for j in range(1, maxStrokePoints):
+                finalDist = la.getDistance(points[i+j].co, points[i+j-1].co)
+
+                if (finalDist < medianDistance):
+                    newPoints.append(points[i+j])
+                else:
+                    if len(newPoints) > 1:
+                        stroke = latk.LatkStroke(newPoints)
+                        strokes.append(stroke)
+                        newPoints = []
+
+            if len(newPoints) > 1:
+                stroke = latk.LatkStroke(newPoints)
+                strokes.append(stroke)
             
-            # MEDIAN
-            for i in range(0, numStrokes):
-                startIndex = int(rnd(0, len(points)))
-                startPoint = points[startIndex]
+        frame = latk.LatkFrame(strokes)
 
-                medianDistance = 0
-
-                for point in points:
-                    point.distance = la.getDistance(point.co, startPoint.co)
-                    if point.distance > medianDistance:
-                        medianDistance = point.distance
-
-                medianDistance /= 2
-
-                points = sorted(points, key=lambda x: getattr(x, 'distance'))
-
-                newPoints = []
-                for j in range(1, maxStrokePoints):
-                    finalDist = la.getDistance(points[i+j].co, points[i+j-1].co)
-
-                    if (finalDist < medianDistance):
-                        newPoints.append(points[i+j])
-                    else:
-                        if len(newPoints) > 1:
-                            stroke = latk.LatkStroke(newPoints)
-                            strokes.append(stroke)
-                            newPoints = []
-
-                if len(newPoints) > 1:
-                    stroke = latk.LatkStroke(newPoints)
-                    strokes.append(stroke)
-                
-            frame = latk.LatkFrame(strokes)
-
-            la.layers[0].frames.append(frame)
+        la.layers[0].frames.append(frame)
 
     la.refine()
 
