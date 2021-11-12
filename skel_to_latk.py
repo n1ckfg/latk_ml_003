60a3d6699ffb5741630638d13dfb540bd8145cb8
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-12 16:15:26 -0500

    Update skel_to_latk.py
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py
    
    Update skel_to_latk.py

--- skel_to_latk.py
+++ skel_to_latk.py
@@ -7,22 +7,36 @@ import binvox_rw
 from random import uniform as rnd
 import pymeshlab as ml
 
-def meshToVoxels(vertices=None, _dims=128): #, axis='xyz'):
-	shape = (_dims, _dims, _dims)
-    data = np.zeros(shape, dtype=bool)
-    #translate = (0, 0, 0)
-    #scale = 1
-    #axis_order = axis
+def scale_numpy_array(arr, min_v, max_v):
+    new_range = (min_v, max_v)
+    max_range = max(new_range)
+    min_range = min(new_range)
 
-    vertices = scale_numpy_array(vertices, 0, _dims - 1)
+    scaled_unit = (max_range - min_range) / (np.max(arr) - np.min(arr))
+    return arr * scaled_unit - np.min(arr) * scaled_unit + min_range
+
+def scaleVertices(vertices, _dims=128):
+    return scale_numpy_array(vertices, 0, _dims - 1)
+    '''
+    for vert in vertices:
+        x = int(vert[0])
+        #x = _dims - 1 - int(vert[0])
+        y = int(vert[1])
+        #y = _dims - 1 - int(vert[1])
+        z = int(vert[2])
+        #z = _dims - 1 - int(vert[2])
+    return vertices
+    '''
+
+def meshToVoxels(vertices=None, _dims=128): #, axis='xyz'):
+    shape = (_dims, _dims, _dims)
+    data = np.zeros(shape, dtype=bool)
 
     for vert in vertices:
-        x = _dims - 1 - int(vert[0])
+        x = int(vert[0])
         y = int(vert[1])
         z = int(vert[2])
         data[x][y][z] = True
-
-    #bv = binvox_rw.Voxels(data, shape, translate, scale, axis_order)
 
     return data
 
@@ -61,45 +75,110 @@ def main():
     edgeUrls.sort()
 
     if (len(colorUrls) == len(coreUrls) == len(edgeUrls)):
-	    la = latk.Latk()
-	    layer = latk.LatkLayer()
-	    la.layers.append(layer)
-	    for i in range(0, len(colorUrls)):
-	    	frame = latk.LatkFrame(frame_number=i)
-	    	la.layers[0].frames.append(frame)
+        la = latk.Latk()
+        layer = latk.LatkLayer()
+        la.layers.append(layer)
+        for i in range(0, len(colorUrls)):
+            frame = latk.LatkFrame(frame_number=i)
+            la.layers[0].frames.append(frame)
 
-		skel = skeletonize(speed_power=1.2, Euler_step_size=0.5, depth_th=2, length_th=None, simple_path=False, verbose=True)
+        skel = skeletonize(speed_power=1.2, Euler_step_size=0.5, depth_th=2, length_th=None, simple_path=False, verbose=True)
 
-	    for i in range(0, len(colorUrls)):
-        	colorMs = ml.MeshSet()
-        	colorMs.load_new_mesh(colorUrls[i])
-        	colorMesh = colorMs.current_mesh()
-        	colorVertices = colorMesh.vertex_matrix()
+        for i in range(0, len(colorUrls)):
+            print("\nLoading meshes " + str(i+1) + " / " + str(len(colorUrls)))
+            colorMs = ml.MeshSet()
+            colorMs.load_new_mesh(colorUrls[i])
+            colorMesh = colorMs.current_mesh()
+            colorVertices = colorMesh.vertex_matrix()
+            colorVertices = scaleVertices(colorVertices, dims)
 
-        	coreMs = ml.MeshSet()
-        	coreMs.load_new_mesh(coreUrls[i])
-        	coreMesh = coreMs.current_mesh()
-        	coreVertices = coreMesh.vertex_matrix()
+            coreMs = ml.MeshSet()
+            coreMs.load_new_mesh(coreUrls[i])
+            coreMesh = coreMs.current_mesh()
+            coreVertices = coreMesh.vertex_matrix()
+            coreVertices = scaleVertices(coreVertices, dims)
 
-        	edgeMs = ml.MeshSet()
-        	edgeMs.load_new_mesh(edgeUrls[i])
-        	edgeMesh = edgeMs.current_mesh()
-        	edgeVertices = edgeMesh.vertex_matrix()
+            edgeMs = ml.MeshSet()
+            edgeMs.load_new_mesh(edgeUrls[i])
+            edgeMesh = edgeMs.current_mesh()
+            edgeVertices = edgeMesh.vertex_matrix()
+            edgeVertices = scaleVertices(edgeVertices, dims)
 
-			colorSk = skel.skeleton(meshToVoxels(coreVertices, dims))
+            print("\nCore skeleton " + str(i+1) + " / " + str(len(colorUrls)))
+            coreSk = skel.skeleton(meshToVoxels(coreVertices, dims))
 
-			for limb in colorSk:
-				points = []
-				for point in limb:
-					point = latk.LatkPoint((point[0], point[2], point[1]))
-					points.append(point)
-				stroke = latk.LatkStroke(points)
-				la.layers[0].frames[i].strokes.append(stroke)
+            for limb in coreSk:
+                points = []
+                for point in limb:
+                    point = latk.LatkPoint((point[0], -point[2], point[1]))
+                    points.append(point)
+                stroke = latk.LatkStroke(points)
+                la.layers[0].frames[i].strokes.append(stroke)
 
-			#la.refine()
+            print("\nEdge skeleton " + str(i+1) + " / " + str(len(colorUrls)))
+            edgeSk = skel.skeleton(meshToVoxels(edgeVertices, dims))
 
-			la.write("output.latk")
-	else:
-		print("Couldn't find equal numbers of assets.")
+            for limb in edgeSk:
+                points = []
+                for point in limb:
+                    point = latk.LatkPoint((point[0], -point[2], point[1]))
+                    points.append(point)
+                stroke = latk.LatkStroke(points)
+                la.layers[0].frames[i].strokes.append(stroke)
+
+            '''
+            print("\nEdge detail " + str(i+1) + " / " + str(len(colorUrls)))
+            
+            maxStrokePoints = 60
+            minStrokePoints = 3
+
+            points = []
+            for vert in coreVertices:
+                point = latk.LatkPoint((vert[0], vert[2], vert[1]))
+                points.append(point)
+
+            numStrokes = int(len(points) / maxStrokePoints)
+            strokes = []
+            
+            # MEDIAN
+            for j in range(0, numStrokes):
+                startIndex = int(rnd(0, len(points)))
+                startPoint = points[startIndex]
+
+                medianDistance = 0
+
+                for point in points:
+                    point.distance = la.getDistance(point.co, startPoint.co)
+                    if point.distance > medianDistance:
+                        medianDistance = point.distance
+
+                medianDistance /= 2
+
+                points = sorted(points, key=lambda x: getattr(x, 'distance'))
+
+                newPoints = []
+                for k in range(1, maxStrokePoints):
+                    finalDist = la.getDistance(points[j+k].co, points[j+k-1].co)
+
+                    if (finalDist < medianDistance):
+                        newPoints.append(points[j+k])
+                    else:
+                        if len(newPoints) > minStrokePoints:
+                            stroke = latk.LatkStroke(newPoints)
+                            strokes.append(stroke)
+                            newPoints = []
+
+                if len(newPoints) > minStrokePoints:
+                    stroke = latk.LatkStroke(newPoints)
+                    strokes.append(stroke)            
+
+            la.layers[0].frames[i].strokes.append(stroke)
+        '''
+        print("\nWriting latk...")
+        #la.refine()
+        la.write("output.latk")
+        print("\n...Finished writing latk.")
+    else:
+        print("Couldn't find equal numbers of assets.")
 
 main()
