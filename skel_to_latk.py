474094d5d9daa8f214ffc677d10dc6bf7e2337fe
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-14 12:36:01 -0500

    Update skel_to_latk.py

--- skel_to_latk.py
+++ skel_to_latk.py
@@ -68,18 +68,17 @@ def main():
         #coreVertices = scaleVertices(coreVertices, dims)
         coreColors = coreMesh.vertex_color_matrix()
 
-        ms.add_mesh(coreMesh)
+        ms.load_new_mesh(urls[i])
         ms.surface_reconstruction_ball_pivoting()
         ms.select_crease_edges()
         ms.build_a_polyline_from_selected_edges()
 
+        ms.vertex_attribute_transfer(sourcemesh=0, targetmesh=1)
         edgeMesh = ms.current_mesh()
         edgeVertices = edgeMesh.vertex_matrix()
         #edgeVertices = scaleVertices(edgeVertices, dims)
         edgeEdges = edgeMesh.edge_matrix()
-
-        ms.apply_filter("vertex_attribute_transfer", sourcemesh=0, targetmesh=1)
-        edgeColors = coreMesh.vertex_color_matrix()
+        edgeColors = edgeMesh.vertex_color_matrix()
 
         if (doSkeleton == True):
             print("\nCore skeleton " + str(i+1) + " / " + str(len(urls)))
