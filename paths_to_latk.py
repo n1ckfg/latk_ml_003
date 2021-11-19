66fbfa1544d82121a57df37d98454afd596ec837
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-19 03:58:36 -0500

    Update paths_to_latk.py

--- paths_to_latk.py
+++ paths_to_latk.py
@@ -47,17 +47,17 @@ def main():
         ms = ml.MeshSet()
         ms.load_new_mesh(urls[i]) # first mesh -> index 0
         coreMesh = ms.current_mesh()
-        #coreVertices = coreMesh.vertex_matrix()
-        #coreVertices = scaleVertices(coreVertices, dims)
 
         ms.add_mesh(coreMesh) # duplicates the current mesh -> index 1
         ms.surface_reconstruction_ball_pivoting()
-        ms.vertex_attribute_transfer(sourcemesh=0, targetmesh=1)
+        ms.select_crease_edges()
+        ms.build_a_polyline_from_selected_edges() # this command creates a new mesh -> index 2
+        ms.surface_reconstruction_ball_pivoting()
+
+        ms.vertex_attribute_transfer(sourcemesh=0, targetmesh=2)
         newTempUrl = os.path.abspath(os.path.join(inputPath, "output" + str(i) + ".ply"));
         ms.save_current_mesh(newTempUrl)
-        vertices = ms.current_mesh().vertex_matrix()
-        vertexColors = ms.current_mesh().vertex_color_matrix()
-
+        
         print("\nSorting edges " + str(i+1) + " / " + str(len(urls)))
         # https://trimsh.org/examples/shortest.html
         mesh = trimesh.load(newTempUrl)
@@ -72,33 +72,38 @@ def main():
 
         print("\nConnecting edge points " + str(i+1) + " / " + str(len(urls)))     
         
-        numStrokes = 10
-        maxStrokePoints = 10
+        vertices = scaleVertices(mesh.vertices, dims)
+
+        numStrokes = 2000
+        minStrokePoints = 5
 
         for j in range(0, numStrokes):
+            points = []
+            start = int(rnd(0, len(mesh.vertices)-1))
+            end = start + int(rnd(-2, 2))
+            
             try:
-                start = int(rnd(maxStrokePoints, len(mesh.vertices) - maxStrokePoints))
-                end = start + int(rnd(-maxStrokePoints, maxStrokePoints))
                 # run the shortest path query using length for edge weight
                 path = nx.shortest_path(g, source=start, target=end, weight='length')
 
-                points = []
                 for index in path:
-                    vert = mesh.vertices[index]
+                    vert = vertices[index]
+                    
                     col = mesh.visual.vertex_colors[index]
-
+                    col = (float(col[0])/255.0, float(col[1])/255.0, float(col[2])/255.0, float(col[3])/255.0)
                     # TODO find out why colors are too light
                     col = (col[0] * col[0], col[1] * col[1], col[2] * col[2], col[3])
 
-                    #print(col)
                     newVert = (-vert[0], vert[2], vert[1])
-                    lp = latk.LatkPoint(newVert, vertex_color=(col[0], col[1], col[2], col[3]))
+                    lp = latk.LatkPoint(newVert, vertex_color=col)
                     points.append(lp)
+            except:
+                pass
+
+            if (len(points) >= minStrokePoints):  
                 stroke = latk.LatkStroke(points)
                 la.layers[0].frames[i].strokes.append(stroke)
-                print ("Stroke " + str(j+1) + " / " + str(numStrokes))
-            except:
-                print ("Skipped " + str(j+1) + " / " + str(numStrokes))
+                print ("Stroke " + str(j+1) + " / " + str(numStrokes) + " with " + str(len(points)) + " points")
 
     print("\nWriting latk...")
     la.write("output.latk")
