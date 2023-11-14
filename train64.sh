552469cb1146391867b444c2d1532bdada3f5a09
Nick Fox-Gieg <nick@fox-gieg.com>
2023-11-13 23:44:26 -0500

    shell

---
+++ train64.sh
@@ -0,0 +1,14 @@
+DIMS=64
+START_EPOCH=0
+TOTAL_EPOCHS=51
+BATCH_SIZE=64
+NUM_CPUS=8
+CHECKPOINT_INTERVAL=10
+
+#some quirks:
+#* You must set a checkpoint interval or the model won't be saved.
+#* The exit condition is off by one, so add an extra epoch.
+#* The default 8 threads is probably too high for free tier Colab, but okay for the paid tiers.
+
+python vox2vox/train.py --epoch "$START_EPOCH" --dataset "input64" --img_width "$DIMS" --img_height "$DIMS" --img_depth "$DIMS" --batch_size "$BATCH_SIZE" --n_cpu "$NUM_CPUS" --checkpoint_interval "$CHECKPOINT_INTERVAL" --n_epochs "$TOTAL_EPOCHS"
+
